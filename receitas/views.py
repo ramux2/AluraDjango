097e7510ab81cb0aa62657e38d38d5856f3af120
Guilherme Ramos <guiramos027@gmail.com>
2022-07-12 17:36:40 -0300

    Melhorando Django

--- receitas/views.py
+++ receitas/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+
+    receitas = {
+        1:'lasanha',
+        2:'Sopa de Legumes',
+        3:'Sorvete', 
+    }
+
+    dados = {
+        'nome_das_receitas' : receitas
+    }
+
+    return render(request, 'index.html', dados)
+
+def receita(request):
+    return render(request, 'receita.html')
