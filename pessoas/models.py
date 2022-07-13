85acd2612160686b340eaab5bec7406a3bef6198
Guilherme Ramos <guiramos027@gmail.com>
2022-07-13 19:06:55 -0300

    Autor da receita no site

--- pessoas/models.py
+++ pessoas/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Pessoa(models.Model):
     nome = models.CharField(max_length=100)
     email = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.nome
