4ec97de7d600ac6c86851fc6ad6cd91a2345e812
Guilherme Ramos <guiramos027@gmail.com>
2022-07-18 17:12:10 -0300

    Log in and Sign In

---
+++ usuarios/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+
+def cadastro(request):
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+        if not nome.strip():
+            return redirect('cadastro')
+
+        if not email.strip():
+            return redirect('cadastro')
+
+        if not senha.strip():
+            return redirect('cadastro')
+
+        if senha != senha2:
+            return redirect('cadastro')
+
+        if User.objects.filter(email = email).exists():
+            return redirect('cadastro')
+
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
+        
+        return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
+
+def login(request):
+    return render(request, 'usuarios/login.html')
+
+def logout(request):
+    pass
+
+def dashboard(request):
+    pass
