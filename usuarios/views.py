e0e2df7d1477f1d7dc21f38279c64469a609cb52
Guilherme Ramos <guiramos027@gmail.com>
2022-07-21 17:04:32 -0300

    Receita no Dashboard

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,9 @@
 import email
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import auth
+from receitas.models import Receita
+from receitas.views import receita
 
 def cadastro(request):
     if request.method == 'POST':
@@ -52,9 +54,29 @@ def logout(request):
 
 def dashboard(request):
     if request.user.is_authenticated:
-        return render(request, 'usuarios/dashboard.html')
+        id = request.user.id
+        receitas = Receita.objects.order_by('-data_receita').filter(pessoa=id)
+
+        dados = {
+            'receitas' : receitas
+        }
+
+        return render(request, 'usuarios/dashboard.html', dados)
     else:
         return redirect('index')
         
 def cria_receita(request):
-    return render(request, 'usuarios/cria_receita.html')
+    if request.method == 'POST':
+        nome_receita = request.POST['nome_receita']
+        ingredientes = request.POST['ingredientes']
+        modo_preparo = request.POST['modo_preparo']
+        tempo_preparo = request.POST['tempo_preparo']
+        rendimento = request.POST['rendimento']
+        categoria = request.POST['categoria']
+        foto_receita = request.FILES['foto_receita']
+        user = get_object_or_404(User, pk=request.user.id)
+        receita = Receita.objects.create(pessoa=user,nome_receita=nome_receita,ingredientes=ingredientes,modo_preparo=modo_preparo,tempo_preparo=tempo_preparo,rendimento=rendimento,categoria=categoria,foto_receita=foto_receita)
+        receita.save()
+        return redirect('dashboard')
+    else:
+        return render(request, 'usuarios/cria_receita.html')
